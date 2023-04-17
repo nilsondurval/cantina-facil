12e0e8e6570616853ee4e3450d8283044b6a6ff2
Nilson Durval <durval.nilson@gmail.com>
2023-04-17 19:35:37 -0300

    correcao cookies

--- CantinaFacil.Client/src/cantina-facil/src/environments/environment.prod.ts
+++ CantinaFacil.Client/src/cantina-facil/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  cookies: 'cantinafacil-api.herokuapp.com',
+  cookies: 'cantinafacil-app.herokuapp.com',
   baseUrl: 'https://cantinafacil-api.herokuapp.com',
   pathApi: '/api'
 };
